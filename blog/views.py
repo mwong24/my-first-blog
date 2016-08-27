a49b670a531353c0c6c868e3a549657dcdb95de0
Maribel <pocketmari@gmail.com>
2016-08-27 18:17:49 +0000

    changed the HTML for the site

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf-exemp
+
 # Create your views here.
 
-
-@csrf_exempt
-def my_view(request)
-    return request
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
